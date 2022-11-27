53f42866f0e85da237814d4f310f3e9c09d10962
Alex529 <alexsk529@mail.ru>
2022-11-27 14:13:20 +0500

    First Commit

---
+++ server/index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+import routerAuth from './routes/routeAuth.js';
+import routerMain from './routes/routerMain.js';
+
+dotenv.config()
+const app = express();
+const PORT = process.env.PORT || 5000;
+const mongoPath = 'mongodb+srv://admin:admin@cluster0.s2pg5n5.mongodb.net/task6-messenger?retryWrites=true&w=majority';
+
+app.use(cors());
+app.use(express.json({extended: true}));
+app.use('/api/auth', routerAuth);
+app.use('/api/main', routerMain);
+
+async function start() {
+    try {
+        await mongoose
+            .connect(mongoPath)
+            .then(()=> console.log("DB OK"))
+            .catch((e) => console.log("DB error: ", e))
+
+        app.listen(PORT, () => {
+            console.log(`Server started on port ${PORT}`)
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start();
